cbc87d196df1068cc4ac83f743357a0ee1f81e98
Sylvain SARAVANANE <sylvainsinou78@gmail.com>
2026-02-11 00:49:35 +0100

    feat(KAN-35): Add SQL scripts for data

---
+++ src/main/resources/teams.sql
@@ -0,0 +1,87 @@
+-- ============================================================
+-- ÉQUIPES COUPE DU MONDE 2026 (IDs 1 à 48)
+-- ============================================================
+
+-- GROUPE A
+INSERT INTO teams (id, name, short_name, crest, group_name) VALUES
+                                                                (1, 'Mexique', 'MEX', 'https://flagcdn.com/w320/mx.png', 'Group A'),
+                                                                (2, 'Afrique du Sud', 'RSA', 'https://flagcdn.com/w320/za.png', 'Group A'),
+                                                                (3, 'Corée du Sud', 'KOR', 'https://flagcdn.com/w320/kr.png', 'Group A'),
+                                                                (4, 'Danemark', 'DEN', 'https://flagcdn.com/w320/dk.png', 'Group A');
+
+-- GROUPE B
+INSERT INTO teams (id, name, short_name, crest, group_name) VALUES
+                                                                (5, 'Canada', 'CAN', 'https://flagcdn.com/w320/ca.png', 'Group B'),
+                                                                (6, 'Suisse', 'SUI', 'https://flagcdn.com/w320/ch.png', 'Group B'),
+                                                                (7, 'Qatar', 'QAT', 'https://flagcdn.com/w320/qa.png', 'Group B'),
+                                                                (8, 'Pologne', 'POL', 'https://flagcdn.com/w320/pl.png', 'Group B');
+
+-- GROUPE C
+INSERT INTO teams (id, name, short_name, crest, group_name) VALUES
+                                                                (9, 'Brésil', 'BRA', 'https://flagcdn.com/w320/br.png', 'Group C'),
+                                                                (10, 'Maroc', 'MAR', 'https://flagcdn.com/w320/ma.png', 'Group C'),
+                                                                (11, 'Haïti', 'HAI', 'https://flagcdn.com/w320/ht.png', 'Group C'),
+                                                                (12, 'Écosse', 'SCO', 'https://flagcdn.com/w320/gb-sct.png', 'Group C');
+
+-- GROUPE D
+INSERT INTO teams (id, name, short_name, crest, group_name) VALUES
+                                                                (13, 'États-Unis', 'USA', 'https://flagcdn.com/w320/us.png', 'Group D'),
+                                                                (14, 'Australie', 'AUS', 'https://flagcdn.com/w320/au.png', 'Group D'),
+                                                                (15, 'Paraguay', 'PAR', 'https://flagcdn.com/w320/py.png', 'Group D'),
+                                                                (16, 'République Tchèque', 'CZE', 'https://flagcdn.com/w320/cz.png', 'Group D');
+
+-- GROUPE E
+INSERT INTO teams (id, name, short_name, crest, group_name) VALUES
+                                                                (17, 'Allemagne', 'GER', 'https://flagcdn.com/w320/de.png', 'Group E'),
+                                                                (18, 'Équateur', 'ECU', 'https://flagcdn.com/w320/ec.png', 'Group E'),
+                                                                (19, 'Côte d''Ivoire', 'CIV', 'https://flagcdn.com/w320/ci.png', 'Group E'),
+                                                                (20, 'Curaçao', 'CUW', 'https://flagcdn.com/w320/cw.png', 'Group E');
+
+-- GROUPE F
+INSERT INTO teams (id, name, short_name, crest, group_name) VALUES
+                                                                (21, 'Pays-Bas', 'NED', 'https://flagcdn.com/w320/nl.png', 'Group F'),
+                                                                (22, 'Japon', 'JPN', 'https://flagcdn.com/w320/jp.png', 'Group F'),
+                                                                (23, 'Tunisie', 'TUN', 'https://flagcdn.com/w320/tn.png', 'Group F'),
+                                                                (24, 'Suède', 'SWE', 'https://flagcdn.com/w320/se.png', 'Group F');
+
+-- GROUPE G
+INSERT INTO teams (id, name, short_name, crest, group_name) VALUES
+                                                                (25, 'Belgique', 'BEL', 'https://flagcdn.com/w320/be.png', 'Group G'),
+                                                                (26, 'Égypte', 'EGY', 'https://flagcdn.com/w320/eg.png', 'Group G'),
+                                                                (27, 'Iran', 'IRN', 'https://flagcdn.com/w320/ir.png', 'Group G'),
+                                                                (28, 'Nouvelle-Zélande', 'NZL', 'https://flagcdn.com/w320/nz.png', 'Group G');
+
+-- GROUPE H
+INSERT INTO teams (id, name, short_name, crest, group_name) VALUES
+                                                                (29, 'Espagne', 'ESP', 'https://flagcdn.com/w320/es.png', 'Group H'),
+                                                                (30, 'Uruguay', 'URU', 'https://flagcdn.com/w320/uy.png', 'Group H'),
+                                                                (31, 'Arabie Saoudite', 'KSA', 'https://flagcdn.com/w320/sa.png', 'Group H'),
+                                                                (32, 'Cap-Vert', 'CPV', 'https://flagcdn.com/w320/cv.png', 'Group H');
+
+-- GROUPE I
+INSERT INTO teams (id, name, short_name, crest, group_name) VALUES
+                                                                (33, 'France', 'FRA', 'https://flagcdn.com/w320/fr.png', 'Group I'),
+                                                                (34, 'Sénégal', 'SEN', 'https://flagcdn.com/w320/sn.png', 'Group I'),
+                                                                (35, 'Norvège', 'NOR', 'https://flagcdn.com/w320/no.png', 'Group I'),
+                                                                (36, 'Bolivie', 'BOL', 'https://flagcdn.com/w320/bo.png', 'Group I');
+
+-- GROUPE J
+INSERT INTO teams (id, name, short_name, crest, group_name) VALUES
+                                                                (37, 'Argentine', 'ARG', 'https://flagcdn.com/w320/ar.png', 'Group J'),
+                                                                (38, 'Autriche', 'AUT', 'https://flagcdn.com/w320/at.png', 'Group J'),
+                                                                (39, 'Algérie', 'ALG', 'https://flagcdn.com/w320/dz.png', 'Group J'),
+                                                                (40, 'Jordanie', 'JOR', 'https://flagcdn.com/w320/jo.png', 'Group J');
+
+-- GROUPE K
+INSERT INTO teams (id, name, short_name, crest, group_name) VALUES
+                                                                (41, 'Portugal', 'POR', 'https://flagcdn.com/w320/pt.png', 'Group K'),
+                                                                (42, 'Colombie', 'COL', 'https://flagcdn.com/w320/co.png', 'Group K'),
+                                                                (43, 'Ouzbékistan', 'UZB', 'https://flagcdn.com/w320/uz.png', 'Group K'),
+                                                                (44, 'Jamaïque', 'JAM', 'https://flagcdn.com/w320/jm.png', 'Group K');
+
+-- GROUPE L
+INSERT INTO teams (id, name, short_name, crest, group_name) VALUES
+                                                                (45, 'Angleterre', 'ENG', 'https://flagcdn.com/w320/gb-eng.png', 'Group L'),
+                                                                (46, 'Croatie', 'CRO', 'https://flagcdn.com/w320/hr.png', 'Group L'),
+                                                                (47, 'Ghana', 'GHA', 'https://flagcdn.com/w320/gh.png', 'Group L'),
+                                                                (48, 'Panama', 'PAN', 'https://flagcdn.com/w320/pa.png', 'Group L');
