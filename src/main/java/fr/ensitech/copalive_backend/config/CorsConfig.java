4f389a870054d120742fe8cea9acb7db92595ae1
Sylvain SARAVANANE <sylvainsinou78@gmail.com>
2026-02-11 00:54:11 +0100

    feat(KAN-36): Add CORS Configuration

---
+++ src/main/java/fr/ensitech/copalive_backend/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package fr.ensitech.copalive_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://127.0.0.1:5173"
+                )
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .exposedHeaders("Authorization")
+                .maxAge(3600);
+    }
+}
