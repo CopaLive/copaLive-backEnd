86cce8454958a8a3ea61fa6d7bc7e05236fd34d4
Sylvain SARAVANANE <sylvainsinou78@gmail.com>
2026-02-11 15:04:55 +0100

    feat(KAN-40): Add SecurityConfig

---
+++ src/main/java/fr/ensitech/copalive_backend/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package fr.ensitech.copalive_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // 1. L'outil qui va crypter les mots de passe
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /*
+    * Mode permissive pour ne pas bloquer l'API
+    * */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+}
