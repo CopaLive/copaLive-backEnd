97244e383072c7713365375d2e1ee48e5c47b920
Sylvain SARAVANANE <sylvainsinou78@gmail.com>
2026-02-11 12:06:39 +0100

    feat(KAN-39): Add areas.sql

---
+++ src/main/resources/areas.sql
@@ -0,0 +1,11 @@
+-- 1. États-Unis (Pays Hôte Principal)
+INSERT INTO areas (name, country_code, flag)
+VALUES ('États-Unis - New York', 'USA', 'https://flagcdn.com/w320/us.png');
+
+-- 2. Canada (Co-organisateur)
+INSERT INTO areas (name, country_code, flag)
+VALUES ('Canada - Montreal', 'CAN', 'https://flagcdn.com/w320/ca.png');
+
+-- 3. Mexique (Co-organisateur)
+INSERT INTO areas (name, country_code, flag)
+VALUES ('Mexique - Mexico City', 'MEX', 'https://flagcdn.com/w320/mx.png');
